b2f36399bd4194762bb1cd15ca1755fc58906993
jiaoheyan <1639175754@qq.com>
2018-02-17 20:07:39 +0800

    201802172007

--- app/src/main/java/com/example/lovej/jlovem/MainActivity.java
+++ app/src/main/java/com/example/lovej/jlovem/MainActivity.java
@@ -1,23 +1,32 @@
 package com.example.lovej.jlovem;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.view.animation.TranslateAnimation;
 import android.widget.Button;
+import android.widget.ImageView;
 
 /**
  * Created by Administrator on 2017/12/25 0025.
  */
 
-public class MainActivity extends AppCompatActivity {
-    Button loveY,disloveY;
+public class MainActivity extends AppCompatActivity implements Animation.AnimationListener{
+    Button loveY,disloveY,openUrl;
     boolean isFlag = true;
+    private PrinterTextView mPrinterTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mPrinterTextView = (PrinterTextView) findViewById(R.id.pt_my);
+        mPrinterTextView.setPrintText("草原上有对狮子母子。小狮子问母狮子：“妈，幸福在哪里?”母狮子说：“幸福就在你的尾巴上。”\n" +
+                "　　于是小狮子不断追着尾巴跑，但始终咬不到。母狮子笑道：“傻瓜!幸福不是这样得到的!只要你昂首向前走，幸福就会一直跟随着你!”。", 100, "|");
+        mPrinterTextView.startPrint();
         loveY = (Button)findViewById(R.id.loveY);
         loveY.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -61,5 +70,34 @@ public class MainActivity extends AppCompatActivity {
             }
 
         });
+        openUrl = (Button)findViewById(R.id.openUrl);
+        openUrl.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri = Uri.parse("http://www.jlovel.top");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
+    }
+    public void imageSize(View view) {
+        ImageView image_scale = (ImageView) findViewById(R.id.image_scale);
+        Animation animation = AnimationUtils.loadAnimation(this,R.anim.size);
+        image_scale.startAnimation(animation);
+    }
+
+    @Override
+    public void onAnimationStart(Animation animation) {
+
+    }
+
+    @Override
+    public void onAnimationEnd(Animation animation) {
+
+    }
+
+    @Override
+    public void onAnimationRepeat(Animation animation) {
+
     }
 }
